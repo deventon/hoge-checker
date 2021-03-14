91a75f85eb55c42815f467cda922aefcc0962b6a
Torben Koch <torben.koch@protonmail.com>
2021-03-15 00:33:14 +0100

    green text on price increase, red on decrease
    Signed-off-by: Torben Koch <torben.koch@protonmail.com>

--- pages/api/getDataFromDb.js
+++ pages/api/getDataFromDb.js
@@ -13,10 +13,15 @@ export default async (req, res) => {
     url: "https://kvdb.io/EnEjHiXtEB7h7rnKvWwmZE/lastUpdated",
     method: "get",
   });
+  const priceIncreaseCall = await axios({
+    url: "https://kvdb.io/EnEjHiXtEB7h7rnKvWwmZE/priceIncrease",
+    method: "get",
+  });
 
   return res.status(200).send({
     currentHoge: currentHogeCall.data,
     currentConversion: currentConversionCall.data,
     lastUpdated: lastUpdatedCall.data,
+    priceIncrease: priceIncreaseCall.data,
   });
 };
--- pages/api/saveDataToDb.js
+++ pages/api/saveDataToDb.js
@@ -2,9 +2,17 @@ import dataHelper from "../../utils/dataHelper";
 import axios from "axios";
 
 export default async (req, res) => {
-  const currentHoge = await dataHelper.callEtherscanApi();
-  const currentPrice = await dataHelper.callCoinmarketcapApi();
+  const currentHoge = await dataHelper.callEtherscanApi(); // get current wallet balance
+  const currentPrice = await dataHelper.callCoinmarketcapApi(); // get current conversion ratio
   const lastUpdated = new Date().toString().substr(0, 24);
+
+  // get last conversion ratio
+  const currentConversionCall = await axios({
+    url: "https://kvdb.io/EnEjHiXtEB7h7rnKvWwmZE/currentPrice",
+    method: "get",
+  });
+  const lastPrice = currentConversionCall.data;
+  const priceIncrease = currentPrice > lastPrice ? true : false;
 
   const etherscanConfig = {
     url: `https://kvdb.io/EnEjHiXtEB7h7rnKvWwmZE/currentHoge`,
@@ -33,13 +41,25 @@ export default async (req, res) => {
     data: JSON.stringify(lastUpdated),
   };
 
+  const priceIncreaseConfig = {
+    url: `https://kvdb.io/EnEjHiXtEB7h7rnKvWwmZE/priceIncrease`,
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify(priceIncrease),
+  };
+
   try {
     await axios(etherscanConfig);
     await axios(coinmarketcapConfig);
     await axios(lastUpdatedConfig);
+    await axios(priceIncreaseConfig);
   } catch (ex) {
     console.log(ex);
   }
 
-  return res.status(200).send([currentHoge, currentPrice, lastUpdated]);
+  return res
+    .status(200)
+    .send([currentHoge, currentPrice, lastUpdated, priceIncrease]);
 };
